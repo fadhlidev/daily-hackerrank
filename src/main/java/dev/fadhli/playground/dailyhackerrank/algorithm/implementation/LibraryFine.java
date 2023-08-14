aded2b0965d7e70876acb73f701e7fecdc20467b
Fadhli Syaifullah <me@fadhli.dev>
2023-08-14 08:01:40 +0700

    DAY 38: Library Fine

---
+++ src/main/java/dev/fadhli/playground/dailyhackerrank/algorithm/implementation/LibraryFine.java
@@ -0,0 +1,43 @@
+package dev.fadhli.playground.dailyhackerrank.algorithm.implementation;
+
+import java.io.*;
+
+public class LibraryFine {
+
+    public static int solution(int d1, int m1, int y1, int d2, int m2, int y2) {
+        if (m1 == m2 && y1 == y2 && d1 > d2) return (d1 - d2) * 15;
+        if (y1 == y2 && m1 > m2) return (m1 - m2) * 500;
+        if (y1 > y2) return 10000;
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = solution(d1, m1, y1, d2, m2, y2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
---
+++ src/test/java/dev/fadhli/playground/dailyhackerrank/algorithm/implementation/LibraryFineTest.java
@@ -0,0 +1,14 @@
+package dev.fadhli.playground.dailyhackerrank.algorithm.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryFineTest {
+
+    @Test
+    void sampleTestCase0() {
+        assertEquals(45, LibraryFine.solution(9, 6, 2015, 6, 6, 2015));
+    }
+
+}
